20f483fb0769f869bb3eee7b8305449af6220a4c
Ankit Parida <ankitparida386@gmail.com>
2025-02-14 22:38:25 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@ int main(){
     int a,b;
     char o;
     scanf("%d %d %c",&a,&b,&o);
-    switch(a,b,o){
+    switch(a,b){
         case '+': printf("%d\n",a+b);
            break;
         case '-': printf("%d\n",a-b);
