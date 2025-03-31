e551ee6c59ed2e5d84c5550a5ed285001d288ad4
Ankit Parida <ankitparida386@gmail.com>
2025-03-31 09:15:42 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
+
+int isPalindrome(int num) {
+    int temp = num, reverse = 0;
+
+    // Convert negative numbers to positive
+    if (num < 0) return 0; 
+
+    while (temp > 0) {
+        reverse = reverse * 10 + (temp % 10);
+        temp /= 10;
+    }
+    
+    return (num == reverse);
+}
+
 int main() {
-    int size, arr[100],reverse=0;
+    int size, arr[100];
+
+    // Read the size of the array
     scanf("%d", &size);
+
+    // Read array elements
     for (int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
 
+    // Check for palindrome numbers
     int found_palindrome = 0;
     for (int i = 0; i < size; i++) {
-        int temp = arr[i];
-        while (temp > 0) {
-            reverse = reverse * 10 + (temp % 10);
-            temp /= 10;
+        if (isPalindrome(arr[i])) {
+            printf("YES\n");
+            found_palindrome = 1;
+            break; // Stop at the first palindrome
         }
-        if(arr[i]==reverse){
-           printf("YES");
-           found_palindrome=1;
-          
     }
-    else{
-        printf("NO");
-        found_palindrome=0;
 
-        
+    // If no palindrome is found
+    if (!found_palindrome) {
+        printf("NO\n");
     }
-        
-    }
-    
-    
+
     return 0;
 }
+        
+    
